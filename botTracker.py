52307498e0b83f2133636de96a441813f5d92cef
Raj Tagore <rajtagore780@gmail.com>
2024-05-20 19:28:26 +0100

    fix

--- botTracker.py
+++ botTracker.py
@@ -1,212 +1,159 @@
 import cv2
-import cv2.aruco as aruco
 import numpy as np
 
-ROWS = 10 #Maze dimensions
+ROWS = 10 
 COLS = 10
 
-# Set all to 1111 (all walls) to avoid errors
-def create_maze(rows, cols):
-    return [['1111' for _ in range(cols)] for _ in range(rows)]
+decimal_maze = [
+    [9, 8, 8, 12, 11, 12, 9, 8, 8, 14],
+    [5, 1, 0, 4, 9, 4, 5, 3, 2, 12],
+    [5, 5, 3, 4, 1, 4, 1, 14, 9, 4],
+    [5, 1, 12, 1, 0, 0, 4, 15, 1, 6],
+    [5, 3, 0, 2, 2, 4, 3, 10, 0, 12],
+    [1, 12, 3, 8, 12, 1, 8, 8, 2, 4],
+    [5, 5, 9, 4, 5, 3, 0, 0, 8, 6],
+    [1, 6, 1, 6, 3, 12, 3, 2, 4, 15],
+    [5, 9, 4, 9, 8, 0, 8, 12, 3, 4],
+    [3, 2, 2, 2, 2, 6, 3, 2, 10, 6]
+]
 
-maze = [[0 for _ in range(COLS)] for _ in range(ROWS)]
-maze = create_maze(ROWS, COLS)
+binary_maze = [[bin(num)[2:].zfill(4) for num in row] for row in decimal_maze]
 
-#Row 0
-maze[0][0] = '1001'
-maze[0][1] = '1010'
-maze[0][2] = '1000'
-maze[0][3] = '1100'
-maze[0][4] = '0001'
-maze[0][5] = '1010'
-maze[0][6] = '1000'
-maze[0][7] = '1000'
-maze[0][8] = '1000'
-maze[0][9] = '1100'
+class Node:
+    def __init__(self, maze_coordinates):
+        self.pixel_coordinates = None
+        self.maze_coordinates = maze_coordinates
+        self.parent = None
+        self.children = []
+        self.visited = False
+        
+    def heuristic(self, goal):
+        return abs(self.maze_coordinates[0] - goal[0]) + abs(self.maze_coordinates[1] - goal[1])
+    
+maze_nodes = [[Node((i, j)) for j in range(COLS)] for i in range(ROWS)]
+start_node = maze_nodes[0][4]
+goal_node = maze_nodes[9][5]
+ai_path = []
 
-#Row 1
-maze[1][0] = '0011'
-maze[1][1] = '1100'
-maze[1][2] = '0111'
-maze[1][3] = '0011'
-maze[1][4] = '0110'
-maze[1][5] = '1011'
-maze[1][6] = '0110'
-maze[1][7] = '0001'
-maze[1][8] = '0110'
-maze[1][9] = '0101'
+cell_size = 0
+start_coordinates = (0, 0)
 
-#Row 2
-maze[2][0] = '1001'
-maze[2][1] = '0010'
-maze[2][2] = '1110'
-maze[2][3] = '1101'
-maze[2][4] = '1011'
-maze[2][5] = '1100'
-maze[2][6] = '1011'
-maze[2][7] = '0110'
-maze[2][8] = '1001'
-maze[2][9] = '0100'
-
-#Row 3
-maze[3][0] = '0001'
-maze[3][1] = '1000'
-maze[3][2] = '1110'
-maze[3][3] = '0011'
-maze[3][4] = '1100'
-maze[3][5] = '0101'
-maze[3][6] = '1001'
-maze[3][7] = '1110'
-maze[3][8] = '0101'
-maze[3][9] = '0111'
-
-#Row 4
-maze[4][0] = '0101'
-maze[4][1] = '0011'
-maze[4][2] = '1010'
-maze[4][3] = '1100'
-maze[4][4] = '0101'
-maze[4][5] = '0011'
-maze[4][6] = '0010'
-maze[4][7] = '1100'
-maze[4][8] = '0011'
-maze[4][9] = '1100'
-
-#Row 5
-maze[5][0] = '0101'
-maze[5][1] = '1011'
-maze[5][2] = '1010'
-maze[5][3] = '0110'
-maze[5][4] = '0101'
-maze[5][5] = '1001'
-maze[5][6] = '1010'
-maze[5][7] = '0100'
-maze[5][8] = '1101'
-maze[5][9] = '0101'
-
-#Row 6
-maze[6][0] = '0011'
-maze[6][1] = '1010'
-maze[6][2] = '1100'
-maze[6][3] = '1001'
-maze[6][4] = '0110'
-maze[6][5] = '0101'
-maze[6][6] = '1101'
-maze[6][7] = '0011'
-maze[6][8] = '0100'
-maze[6][9] = '0101'
-
-#Row 7
-maze[7][0] = '1001'
-maze[7][1] = '1000'
-maze[7][2] = '0010'
-maze[7][3] = '0110'
-maze[7][4] = '1001'
-maze[7][5] = '0110'
-maze[7][6] = '0001'
-maze[7][7] = '1100'
-maze[7][8] = '0101'
-maze[7][9] = '0101'
-
-#Row 8
-maze[8][0] = '0111'
-maze[8][1] = '0001'
-maze[8][2] = '1100'
-maze[8][3] = '1101'
-maze[8][4] = '0101'
-maze[8][5] = '1011'
-maze[8][6] = '0100'
-maze[8][7] = '0001'
-maze[8][8] = '0110'
-maze[8][9] = '0101'
-
-#Row 9
-maze[9][0] = '1011'
-maze[9][1] = '0110'
-maze[9][2] = '0011'
-maze[9][3] = '0110'
-maze[9][4] = '0011'
-maze[9][5] = '1100'
-maze[9][6] = '0111'
-maze[9][7] = '0011'
-maze[9][8] = '1010'
-maze[9][9] = '0110'
-
-
-# Test maze has been made correctly
 def print_maze(maze):
     for row in maze:
         print(' '.join(row))
 
-def draw_maze(frame, maze, cell_size=50): #draw lines for 1s
-    maze_height = len(maze)
-    maze_width = len(maze[0])
+# draws the maze in the frame and configures the children of each node
+def process_maze(frame, binary_maze, maze_nodes): 
+    
+    global cell_size, start_coordinates, ROWS, COLS
+    
+    cell_size = int(frame.shape[0]/13)
+    start_coordinates = (int(frame.shape[1]/2 - COLS * cell_size/2), int(frame.shape[0]/2 - ROWS * cell_size/2))
 
-    for i in range(maze_height):
-        for j in range(maze_width):
-            cell = maze[i][j]
-            x = j * cell_size
-            y = i * cell_size
+    for i in range(ROWS):
+        for j in range(COLS):
+            
+            cell = binary_maze[i][j]
+            node = maze_nodes[i][j]
+            x = start_coordinates[0] + j * cell_size
+            y = start_coordinates[1] + i * cell_size
+            node.pixel_coordinates = (int(x + cell_size/2), int(y + cell_size/2))
 
             if cell[0] == '1': #Top wall
-                cv2.line(frame, (x, y), (x + cell_size, y), (0, 0, 0), 2)
+                cv2.line(frame, (x, y), (x + cell_size, y), (0, 255, 255), 2)
+            else:
+                node.children.append(maze_nodes[i - 1][j])
             if cell[1] == '1': #Right wall
-                cv2.line(frame, (x + cell_size, y), (x + cell_size, y + cell_size), (0, 0, 0), 2)
+                cv2.line(frame, (x + cell_size, y), (x + cell_size, y + cell_size), (0, 255, 255), 2)
+            else:
+                node.children.append(maze_nodes[i][j + 1])
             if cell[2] == '1': #Bottom wall
-                cv2.line(frame, (x, y + cell_size), (x + cell_size, y + cell_size), (0, 0, 0), 2)
+                cv2.line(frame, (x, y + cell_size), (x + cell_size, y + cell_size), (0, 255, 255), 2)
+            else:
+                node.children.append(maze_nodes[i + 1][j])
             if cell[3] == '1': #Left wall
-                cv2.line(frame, (x, y), (x, y + cell_size), (0, 0, 0), 2)
+                cv2.line(frame, (x, y), (x, y + cell_size), (0, 255, 255), 2)
+            else:
+                node.children.append(maze_nodes[i][j - 1])
+    return frame
+
+# detects the aruco markers and sets the start and goal nodes
+def detect_aruco_markers(frame):
+    global maze_nodes, start_node, goal_node, ROWS, COLS
+    maze_nodes = [[Node((i, j)) for j in range(COLS)] for i in range(ROWS)]
     
-def botTracker():
-    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250) #For generating markers
-    parameters = cv2.aruco.DetectorParameters() #Parameters for detection
-
-    # Camera matrix and distortion coefficients
-    camera_matrix = np.array([[1000, 0, 640],
-                          [0, 1000, 360],
-                          [0, 0, 1]], dtype=np.float32)
-    dist_coeffs = np.zeros((5, 1), dtype=np.float32)
-    cap = cv2.VideoCapture(0)
-
-    if not cap.isOpened():
-        print("Error: Could not open camera.")
-        return
-
-    while True:
-        ret, frame = cap.read() #Each frame
-
-        if not ret:
-            print("Error: Failed to capture image.")
-            break
-
-        draw_maze(frame, maze) #overlay maze onto feed
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convert to grayscale for reading markers
-
-        corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters) #Detect markers
-
-        if ids is not None and len(ids) > 0: #Marker detected
-            rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners, 0.05, camera_matrix, dist_coeffs) #Find position of marker
+    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+    parameters =  cv2.aruco.DetectorParameters()
+    detector = cv2.aruco.ArucoDetector(dictionary, parameters)
+    corners, ids, _ = detector.detectMarkers(frame)
+    
+    if ids is not None:
+        for _ in range(len(ids)):
+            cv2.aruco.drawDetectedMarkers(frame, corners, ids, (0, 255, 0))
+    
+        for marker_id, corner in zip(ids, corners):
+            top_left, top_right, bottom_right, bottom_left = corner[0][0], corner[0][1], corner[0][2], corner[0][3]
+            center = (int((top_left[0] + bottom_right[0])/2), int((top_left[1] + bottom_right[1])/2))
+            angle = np.arctan2(top_right[1] - top_left[1], top_right[0] - top_left[0]) * 180/np.pi
+            global start_coordinates, cell_size
+            u = int((center[0] - start_coordinates[0])//(cell_size))
+            v = int((center[1] - start_coordinates[1])//(cell_size))
+            u=0 if u<0 else u
+            v=0 if v<0 else v
+            u=9 if u>9 else u
+            v=9 if v>9 else v
         
-            for rvec, tvec in zip(rvecs, tvecs):
-                cv2.aruco.drawAxis(frame, camera_matrix, dist_coeffs, rvec, tvec, 0.1) # Draw axis for each marker
-                cv2.aruco.drawDetectedMarkers(frame, corners) #Outline
+            if marker_id[0] == 0:
+                start_node = maze_nodes[v][u]
+                
+            if marker_id[0] == 1:
+                goal_node = maze_nodes[v][u]
             
-                position = f"Position: x={tvec[0][0]:.2f}, y={tvec[0][1]:.2f}, z={tvec[0][2]:.2f}" #Display info
-                orientation = f"Orientation: rvec={rvec[0][0]:.2f}, {rvec[0][1]:.2f}, {rvec[0][2]:.2f}"
-                cv2.putText(frame, position, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                cv2.putText(frame, orientation, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        else:
-            cv2.putText(frame, "No markers detected", (1100, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    return frame
 
-        cv2.imshow('Camera Feed', frame) #Show frame
+# finds the path from the start node to the goal node
+def update_ai_path(start, goal):
+    open_list = [start]
+    start.visited = True
+    while open_list:
+        current_node = open_list.pop(0)
+        if current_node == goal:
+            path = []
+            while current_node:
+                path.append(current_node)
+                current_node = current_node.parent
+            global ai_path
+            ai_path = path[::-1]
+        if current_node is not None:
+            for child in current_node.children:
+                if not child.visited:
+                    child.parent = current_node
+                    child.visited = True
+                    open_list.append(child)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'): #q to quit
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+# draws the path in the frame
+def draw_path(frame):
+    global ai_path
+    for i in range(len(ai_path) - 1):
+        cv2.line(frame, ai_path[i].pixel_coordinates, ai_path[i + 1].pixel_coordinates, (0, 255, 0), 2)
+    return frame
+    
 
 if __name__ == "__main__":
-    print_maze(maze)
-    botTracker()
+    
+    cap = cv2.VideoCapture(1)
+    
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame = detect_aruco_markers(frame)
+        frame = process_maze(frame, binary_maze, maze_nodes)
+        update_ai_path(start_node, goal_node)
+        frame = draw_path(frame)
+        cv2.imshow("Video Feed", frame)
+        if cv2.waitKey(1) == ord('q'):
+            breakpoint
+            
+    cap.release()
+    cv2.destroyAllWindows()
